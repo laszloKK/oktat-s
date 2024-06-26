bb10d3fcc1e24a54c2cc426a10c9277e5522ea18
laszloKK <laszlokinga89@gmail.com>
2024-06-26 22:51:53 +0200

    Házi második fele és javítások

--- homeWork_01.js
+++ homeWork_01.js
@@ -2,48 +2,80 @@
 let a = 2;
 let b = 3;
 let c = a + b;
+console.log(c);
 
 //2
 let d = 4;
 let e = 6;
 e = d;
 d = 6;
+console.log(d, e);
 
 //3
-let f = [1, 2, 3, 4];
+const f = [1, 2, 3, 4];
 f[0] = 4;
 f[1] = 3;
 f[2] = 2;
 f[3] = 1;
+console.log(f);
 
 //4
-let kulcs = {};
-let kulcs1 = 10;
-let kulcs2 = 20;
-kulcs.push kulcs1;
-kulcs.push kulcs2;
 
 //5
 
-
 //6
-let g = [5, 6, 7, 8, 9];
+const g = [5, 6, 7, 8, 9];
 let h = 4;
 h = g[4];
+console.log(h);
 
 //7
-let a3 = [o1, o2, o3];
-let o1 = {
-    firstname: Andrea,
-    secondname: Arany
-};
-let o2 = {
-    firstname: Barnabás,
-    secondname: Bokor
-};
-let o3 = {
-    firstname: Cecília,
-    secondname: Cérna
-}
 
 //8
+const array9 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+array9[1[1]] = 10;
+console.log(array9[1]);
+
+//10
+const num = [2, 3, 5, 7, 8];
+let sum = num[0] + num[1] + num[2] + num[3] + num[4];
+console.log(sum)
+
+//11
+let i = 4;
+let j = 5;
+let k = 6;
+const array3 = [];
+array3.push(i);
+array3.push(j);
+array3.push(k);
+console.log(array3)
+
+//12
+const address = {
+    pcode: 4024,
+    city: "Debrecen",
+    street: "Akármi",
+    number: 42
+}
+console.log(address);
+
+//13
+let a = 5;
+let b = 7;
+let circumference = 2 * a + 2 * b;
+let area = a * b;
+console.log(circumference);
+console.log(area);
+
+//14
+const less = [1, 2, 3, 4, 5];
+const more = [9, 8, 7, 6, 5];
+const sumsum = [
+    less[0] + more[0],
+    less[1] + more[1],
+    less[2] + more[2],
+    less[3] + more[3],
+    less[4] + more[4],
+];
+console.log(sumsum);
