f4de60b77580c642df70f13cf3a9f8d11382d977
laszloKK <laszlokinga89@gmail.com>
2024-06-26 13:35:17 +0200

    Házi első fele

---
+++ homeWork_01.js
@@ -0,0 +1,49 @@
+//1
+let a = 2;
+let b = 3;
+let c = a + b;
+
+//2
+let d = 4;
+let e = 6;
+e = d;
+d = 6;
+
+//3
+let f = [1, 2, 3, 4];
+f[0] = 4;
+f[1] = 3;
+f[2] = 2;
+f[3] = 1;
+
+//4
+let kulcs = {};
+let kulcs1 = 10;
+let kulcs2 = 20;
+kulcs.push kulcs1;
+kulcs.push kulcs2;
+
+//5
+
+
+//6
+let g = [5, 6, 7, 8, 9];
+let h = 4;
+h = g[4];
+
+//7
+let a3 = [o1, o2, o3];
+let o1 = {
+    firstname: Andrea,
+    secondname: Arany
+};
+let o2 = {
+    firstname: Barnabás,
+    secondname: Bokor
+};
+let o3 = {
+    firstname: Cecília,
+    secondname: Cérna
+}
+
+//8
